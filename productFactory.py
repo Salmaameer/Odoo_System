ce54312ff6e709d340d5b72014de87acfcf2d080
salma ameer <salmaameer409@gmail.com>
2025-11-24 23:44:45 +0200

    add product model

---
+++ productFactory.py
@@ -0,0 +1,9 @@
+from product import Product
+
+class ProductFactory():
+    @staticmethod
+    def create(name, price, description, quantity):
+        return Product(name, price, description, quantity)
+    
+
+
